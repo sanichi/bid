e8f37ac4d6bff8cbdc3b9de4bffa2627231df719
Mark Orr <mark.j.l.orr@googlemail.com>
2022-03-10 14:05:56 +0000

    Security overhaul

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 Rails.application.config.session_store :cookie_store,
-  key: "_bridge_app_session",
+  key: "#{Rails.env.production? ? '__Secure-' : '_'}Bridge_Session",
   expire_after: 4.weeks
