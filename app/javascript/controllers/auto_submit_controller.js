302016eabfe88782173e17a13be46d71fd0b31c0
Mark Orr <mark.j.l.orr@googlemail.com>
2025-01-17 12:57:48 +0000

    Better implementation of auto-submit

--- app/javascript/controllers/auto_submit_controller.js
+++ app/javascript/controllers/auto_submit_controller.js
@@ -2,11 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    const form = this.element;
-    form.querySelectorAll(':scope select.auto-submit').forEach( (item) => {
-      item.addEventListener("change", function () {
-        form.requestSubmit();
-      });
+    this.element.addEventListener("change", function (e) {
+      this.form.requestSubmit();
     });
   }
 }
